6aed75c88e9031a785f9639190670e9fc06eb283
KK <dev-kk@oeaw.ac.at>
2023-11-10 11:56:17 +0100

    build: adapt urls

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from apis_acdhch_default_settings.urls import urlpatterns
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
+urlpatterns += [
+    # path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
