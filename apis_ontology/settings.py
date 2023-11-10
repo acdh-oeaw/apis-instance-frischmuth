b6cbed5316615a08a037b02764a2b60aa01a3c3d
KK <dev-kk@oeaw.ac.at>
2023-11-10 16:31:47 +0100

    ci: use env variable for ALLOWED_HOSTS
    Remove hard-coded domain name from ALLOWED_HOSTS settings
    as value is meant to be set via env variable for deployments.

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -142,7 +142,6 @@ DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "apis-instance-frischmuth.acdh-dev.oeaw.ac.at",
 ]
 
 if "ALLOWED_HOSTS" in os.environ:
