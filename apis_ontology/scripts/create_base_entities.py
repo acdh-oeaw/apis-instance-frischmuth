9d2d271bcde6180697e30d5f51485a2c096b2719
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-01-04 11:56:51 +0000

    refactor: use most recent models and fields

--- apis_ontology/scripts/create_base_entities.py
+++ apis_ontology/scripts/create_base_entities.py
@@ -1,6 +1,6 @@
 import os
 from apis_core.apis_relations.models import Property
-from apis_ontology.models import Archive, Person, Type
+from apis_ontology.models import Archive, Person, WorkType
 from .additional_infos import ARCHIVES, PERSONS, WORK_TYPES
 from .import_helpers import create_triple, create_source
 
@@ -25,10 +25,10 @@ def create_archives(calling_file=fname):
     for a in ARCHIVES:
         # for archives, save XML file as pubinfo when creating sources
         # for later reference
-        source = create_source(import_name, metadata=a["source_file"])
+        source, created = create_source(import_name, metadata=a["source_file"])
         Archive.objects.get_or_create(
             name=a["name"],
-            defaults={"source": source},
+            defaults={"data_source": source},
         )
 
 
@@ -40,42 +40,42 @@ def create_persons(calling_file=fname):
                          script, otherwise uses this file's name
     """
     import_name = "Persons_Import"
-    source = create_source(import_name, metadata=calling_file)
+    source, created = create_source(import_name, metadata=calling_file)
 
     for p in sorted(PERSONS, key=lambda d: d["id"]):
         Person.objects.get_or_create(
             name=p["name"],
             first_name=p["first_name"],
             last_name=p["last_name"],
-            defaults={"source": source},
+            defaults={"data_source": source},
         )
 
 
 def create_types(calling_file=fname):
     """
-    Create objects for Type entity.
+    Create objects for WorkType entity.
 
     :param calling_file: optional argument to pass filename of the calling
                          script, otherwise uses this file's name
     """
-    import_name = "Types_Import"
-    source = create_source(import_name, metadata=calling_file)
+    import_name = "WorkTypes_Import"
+    source, created = create_source(import_name, metadata=calling_file)
 
     # types with parents, not top-level types
     children = {key: val for (key, val) in WORK_TYPES.items() if val["parent_key"]}
 
     # create objects for all types
     for work_type in WORK_TYPES.values():
-        wtype, created = Type.objects.get_or_create(
+        wtype, created = WorkType.objects.get_or_create(
             name=work_type["german_label"],
             name_plural=work_type["german_label_plural"],
-            defaults={"source": source},
+            defaults={"data_source": source},
         )
 
     for work_type in children.values():
-        wt_object = Type.objects.get(name=work_type["german_label"])
+        wt_object = WorkType.objects.get(name=work_type["german_label"])
         parent_key = work_type["parent_key"]
-        parent_object = Type.objects.get(name=WORK_TYPES[parent_key]["german_label"])
+        parent_object = WorkType.objects.get(name=WORK_TYPES[parent_key]["german_label"])
         create_triple(
             entity_subj=wt_object,
             entity_obj=parent_object,
