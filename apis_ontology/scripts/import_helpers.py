c08e62ff45de65fcad98550315d90efbe22fd591
KK <dev-kk@oeaw.ac.at>
2024-01-12 14:18:57 +0100

    refactor: more references to DataSource, WorkType

--- apis_ontology/scripts/import_helpers.py
+++ apis_ontology/scripts/import_helpers.py
@@ -3,9 +3,16 @@ import inspect
 import os
 import sys
 
-from apis_core.apis_metainfo.models import Source
 from apis_core.apis_relations.models import TempTriple
-from apis_ontology.models import Work, Expression, Person, Organisation, Place, Type
+from apis_ontology.models import (
+    DataSource,
+    Expression,
+    Organisation,
+    Person,
+    Place,
+    Work,
+    WorkType,
+)
 from .utils import create_import_date_string, convert_year_only_date
 
 
@@ -31,21 +38,21 @@ def create_source(
     with_date: bool = True,
 ):
     """
-    Helper function for creating a Source object by which
+    Helper function for creating a DataSource object by which
     individual data imports can be identified.
 
     Useful for e.g. later removal of objects created during
     specific imports.
 
     :param name: base name of import source,
-                 used for Source "orig_filename" field
+                 used for DataSource "file_name" field
     :param separator: word separator
     :param metadata: should hold more information about import source, defaults
-                     to name of calling script if not provided; used for Source
-                     "pubinfo" field
-    :param author: used for Source "author" field
+                     to name of calling script if not provided; used for
+                     DataSource "pubinfo" field
+    :param author: used for DataSource "author" field
     :param with_date: boolean, determines if datetime of import script
-                      execution should be appended to Source name
+                      execution should be appended to DataSource name
     :return:
     """
     # script from which create_source is called
@@ -59,11 +66,11 @@ def create_source(
         metadata = calling_script
 
     if not author:
-        # required field for Source, cannot be empty
+        # required field for DataSource, cannot be empty
         author = ""
 
-    source_obj, created = Source.objects.get_or_create(
-        orig_filename=name,
+    source_obj, created = DataSource.objects.get_or_create(
+        file_name=name,
         pubinfo=metadata,
         author=author,
     )
@@ -71,20 +78,20 @@ def create_source(
     return source_obj, created
 
 
-def create_work(title: str, siglum: str, source: Source):
+def create_work(title: str, siglum: str, source: DataSource):
     """
     Create a new Work entity object if one with the given parameters
     does not exist yet.
 
     :param title: main title of the Work, used for "name" field
     :param siglum: unique identifier for Work objects
-    :param source: Source object to link to, used to identify data imports
+    :param source: DataSource object to link to, used to identify data imports
     :return: Work object
     """
     work, created = Work.objects.get_or_create(
         siglum=siglum,
         name=title,
-        defaults={"source": source},
+        defaults={"data_source": source},
     )
 
     return work, created
@@ -108,12 +115,12 @@ def get_work(siglum: str):
 
 def get_type(title: str):
     """
-    :return: (Work) Type object or None
+    :return: WorkType object or None
     """
     work_type = None
 
     try:
-        work_type = Type.objects.get(
+        work_type = WorkType.objects.get(
             name=title,
         )
     except Exception as e:
@@ -123,7 +130,11 @@ def get_type(title: str):
 
 
 def create_expression(
-    title: str, pub_date: str, source: Source, pages: int = None, man_types: list = None
+    title: str,
+    pub_date: str,
+    source: DataSource,
+    pages: int = None,
+    man_types: list = None,
 ):
     """
     Create a new Expression entity object if one with the given parameters
@@ -131,7 +142,7 @@ def create_expression(
 
     :param title: full title of the Expression, used for "name" field
     :param pub_date: date object
-    :param source: Source object to link to, used to identify data imports
+    :param source: DataSource object to link to, used to identify data imports
     :param pages: number of pages
     :param man_types: type(s) of manifestation, if any; can be string
                     or list of strings
@@ -142,13 +153,13 @@ def create_expression(
         manifestation_type=man_types,
         year_of_publication=pub_date,
         page_count=pages,
-        defaults={"source": source},
+        defaults={"data_source": source},
     )
 
     return expression, created
 
 
-def create_person(person_data: dict, source: Source):
+def create_person(person_data: dict, source: DataSource):
     """
     Create a new Person entity object if one with the given parameters
     does not exist yet.
@@ -157,7 +168,7 @@ def create_person(person_data: dict, source: Source):
 
     :param person_data: dict containing keys/values holding information
                         about a person's name
-    :param source: Source object to link to, used to identify data imports
+    :param source: DataSource object to link to, used to identify data imports
     :return: Person object
     """
     # Zotero uses fields "lastName" and "firstName" for creators
@@ -196,13 +207,13 @@ def create_person(person_data: dict, source: Source):
         name=full_name,
         first_name=first_name,
         last_name=last_name,
-        defaults={"source": source},
+        defaults={"data_source": source},
     )
 
     return person, created
 
 
-def create_organisation(org_name: str, source: Source):
+def create_organisation(org_name: str, source: DataSource):
     """
     Create a new Organisation entity object if one with the given parameters
     does not exist yet.
@@ -210,18 +221,18 @@ def create_organisation(org_name: str, source: Source):
     Primarily used to create new publishers.
 
     :param org_name: name of an organisation, used for "name" field
-    :param source: Source object to link to, used to identify data imports
+    :param source: DataSource object to link to, used to identify data imports
     :return: Organisation object
     """
     organisation, created = Organisation.objects.get_or_create(
         name=org_name,
-        defaults={"source": source},
+        defaults={"data_source": source},
     )
 
     return organisation, created
 
 
-def create_place(place_name: str, source: Source, place_type: str = None):
+def create_place(place_name: str, source: DataSource, place_type: str = None):
     """
     Create a new Place entity object if one with the given parameters
     does not exist yet.
@@ -230,7 +241,7 @@ def create_place(place_name: str, source: Source, place_type: str = None):
     publishers.
 
     :param place_name: name of a place, used for "name" field
-    :param source: Source object to link to, used to identify data imports
+    :param source: DataSource object to link to, used to identify data imports
     :param place_type: can be used to addd notes about a place, e.g. to
                        add a separate place object for a place of publication
                        or a publisher's address; gets added to "notes" field
@@ -238,7 +249,7 @@ def create_place(place_name: str, source: Source, place_type: str = None):
     """
     place, created = Place.objects.get_or_create(
         name=place_name,
-        defaults={"source": source, "notes": place_type},
+        defaults={"data_source": source, "notes": place_type},
     )
 
     return place, created
