821d9880dbf50e5c8b7f4f608e03cd4b37b3c961
KK <dev-kk@oeaw.ac.at>
2024-04-15 15:06:22 +0200

    feat(api): add minimal first custom endpoint
    - return title, subtitle for Work objects with
      siglum value, without requiring authentication
    - use custom LimitOffset pagination

---
+++ apis_ontology/api/views.py
@@ -0,0 +1,34 @@
+"""
+Views for custom API.
+
+I.e. project-specific endpoints (not APIS built-in API).
+"""
+
+from rest_framework import permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from apis_ontology.models import Work
+from .serializers import WorkPreviewSerializer
+
+
+class WorkPreviewPagination(LimitOffsetPagination):
+    default_limit = 20
+    max_limit = 100
+
+
+class WorkPreviewViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint which returns minimal, partial Work objects
+    enriched with data from other, related entities.
+
+    The full result set is meant to populate the default view of the
+    "Search" page on the Vue.js frontend (initial view, view when
+    search has been cleared).
+    """
+
+    queryset = (
+        Work.objects.all().filter(siglum__isnull=False).order_by("title", "subtitle")
+    )
+    serializer_class = WorkPreviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = WorkPreviewPagination
