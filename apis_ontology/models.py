d8152b8982dce7167d9a5a21d22a4d9ada233bba
KK <dev-kk@oeaw.ac.at>
2024-06-05 09:54:45 +0200

    feat(signals): update fields of duplicate objects
    Add post_duplicate signal to manipulate fields/field contents
    of entity objects created as copies of existing entity
    objects (via duplicate button).

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -13,6 +13,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from multiselectfield import MultiSelectField
 
+from .signals import update_duplicates  # noqa
+
 
 logger = logging.getLogger(__name__)
 
