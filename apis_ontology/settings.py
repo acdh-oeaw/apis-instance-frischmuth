5b7fcbebb8b99f69863ae4bc96699528a09cdb65
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-01-22 14:27:07 +0100

    fix: open up API for readonly

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -137,7 +137,8 @@ ALLOWED_CIDR_NETS = ["10.0.0.0/8", "127.0.0.0/8"]
 
 
 REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = (
-    "rest_framework.permissions.IsAuthenticated",
+    #"rest_framework.permissions.IsAuthenticated",
+    "rest_framework.permissions.IsAuthenticatedOrReadOnly
 )
 
 # drf-spectacular
